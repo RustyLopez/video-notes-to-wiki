0c522d0f1f7698a5a513e82c62f11546ea11b041
Rusty Lopez <rustylopez@gmail.com>
2026-04-17 11:13:10 -0600

    Initial

---
+++ src/main/java/com/chaostensor/video_notes_to_wiki/VideoNotesToWikiApplication.java
@@ -0,0 +1,13 @@
+package com.chaostensor.video_notes_to_wiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoNotesToWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoNotesToWikiApplication.class, args);
+	}
+
+}
